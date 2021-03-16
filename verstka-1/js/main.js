d7c021641293a04b69f874abc9e7b73befcce5d1
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-03-16 16:27:29 +0200

    js

---
+++ verstka-1/js/main.js
@@ -0,0 +1,28 @@
+// let a = 10;
+
+// a = prompt("scrie ceva");
+// if (a == 10) {
+//     alert('a = 10');
+// } else {
+//     alert("alt ceva");
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     alert(i);
+// }
+
+
+
+let user = {
+    'name': 'Iulian',
+    'surname': 'Preasca',
+    'age': 18
+}
+// alert(user.age);
+
+let mas = [
+    'Iulian',
+    'Preasca',
+    25
+];
+alert(mas[0]);
